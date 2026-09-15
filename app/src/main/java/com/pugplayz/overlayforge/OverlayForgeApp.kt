11def8e0e8128ce4929a94acd2f8462c252ac580
pugplayzYT <pugplayzthesecond@gmail.com>
2026-09-15 15:41:43 +0930

    fix: use scoped Compose weight modifiers

--- app/src/main/java/com/pugplayz/overlayforge/OverlayForgeApp.kt
+++ app/src/main/java/com/pugplayz/overlayforge/OverlayForgeApp.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
